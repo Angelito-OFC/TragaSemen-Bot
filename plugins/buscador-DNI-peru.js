316d58b22e26957169b10504e9df43f2fbeac0b4
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-04-12 22:42:37 -0400

    Actualizar buscador-DNI-peru.js

--- plugins/buscador-DNI-peru.js
+++ plugins/buscador-DNI-peru.js
@@ -1,42 +1,62 @@
+
+import {googleIt} from '@bochilteam/scraper';
+import google from 'google-it';
 import axios from 'axios';
-import fetch from 'node-fetch';
-import cheerio from 'cheerio';
-async function dni(querry) {
-  try {
-    const link = await axios.get(`https://el-dni.com/dni/${querry}`);
-    const $ = cheerio.load(link.data);
-    const judul = $('#firstHeading').text().trim();
-    const thumb = $('#mw-content-text').find('div.mw-parser-output > div:nth-child(1) > table > tbody > tr:nth-child(2) > td > a > img').attr('src') || `//i.ibb.co/nzqPBpC/http-error-404-not-found.png`;
-    const isi = [];
-    $('#mw-content-text > div.mw-parser-output').each(function(rayy, Ra) {
-      const penjelasan = $(Ra).find('p').text().trim();
-      isi.push(penjelasan);
-    });
-    for (const i of isi) {
-      const data = {
-        status: link.status,
-        result: {
-          judul: judul,
-          thumb: 'https:' + thumb,
-          isi: i}};
-      return data;
-    }
-  } catch (err) {
-    const notFond = {
-      status: link.status,
-      Pesan: eror};
-    return notFond;
-  }
-}
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗️𝐈𝐍𝐅𝐎❗️] 𝙴𝚂𝚃𝙰𝚂 𝚄𝚂𝙰𝙽𝙳𝙾 𝙼𝙰𝙻 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾!!*\n*𝚄𝚂𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙾:*\n*${usedPrefix + command} 𝙴𝚕 𝚍𝚗𝚒 𝚙𝚎𝚛𝚞𝚊𝚗𝚘 𝚊 𝚋𝚞𝚜𝚌𝚊𝚛*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} 06256217*`;
-  dni(`${text}`).then((res) => {
-    m.reply(`*𝙰𝚀𝚄𝙸 𝚃𝙸𝙴𝙽𝙴𝚂 𝙻𝙰 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙰𝙳𝙰:*\n\n` + res.result.isi);
-  }).catch(() => {
-    m.reply('*[❗️𝐈𝐍𝐅𝐎❗️] 𝙽𝙾 𝚂𝙴 𝙴𝙽𝙲𝙾𝙽𝚃𝚁𝙾 𝙽𝙸𝙽𝙶𝚄𝙽𝙰 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝙲𝙸𝙾𝙽, 𝙿𝚁𝚄𝙴𝙱𝙰 𝚀𝚄𝙴 𝙷𝙰𝚈𝙰𝚂 𝙴𝚂𝙲𝚁𝙸𝚃𝙾 𝚄𝙽𝙰 𝚂𝙾𝙻𝙰 𝙿𝙰𝙻𝙰𝙱𝚁𝙰 𝚈 𝙻𝙾 𝙷𝙰𝚈𝙰𝚂 𝙴𝚂𝙲𝚁𝙸𝚃𝙾 𝙲𝙾𝚁𝚁𝙴𝙲𝚃𝙰𝙼𝙴𝙽𝚃𝙴*');
-  });
-};
-handler.help = ['dni'].map((v) => v + ' <apa>');
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+  const fetch = (await import('node-fetch')).default;
+  const text = args.join` `;
+  if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m);
+const url = 'https://google.com/search?q=' + encodeURIComponent(text);
+google({'query': text}).then(res => {
+let teks = `*RESULTADOS DE : _${text}_*\n\n${url}\n\n`
+for (let g of res) {
+teks += `_*${g.title}*_\n_${g.link}_\n_${g.snippet}_\n\n`
+} 
+const ss = `https://image.thum.io/get/fullpage/${url}`
+conn.sendFile(m.chat, ss, 'error.png', teks, m)
+//m.reply(teks)
+})
+} 
+handler.help = ['dni', 'dni'].map((v) => v + ' <pencarian>');
 handler.tags = ['internet'];
-handler.command = /^(dni|dni)$/i;
+handler.command = /^dni?$/i;
 export default handler;
+
+/*import {googleIt} from '@bochilteam/scraper';
+import axios from 'axios';
+const handler = async (m, {conn, command, args}) => {
+  const fetch = (await import('node-fetch')).default;
+  const text = args.join` `;
+  if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝚃𝙴𝚇𝚃𝙾 𝙾 𝚃𝙴𝙼𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙱𝚄𝚂𝙲𝙰𝚁*', m);
+  const url = 'https://google.com/search?q=' + encodeURIComponent(text);
+  const search = await googleIt(text);
+  const msg = search.articles.map(({title, url, description}) => {
+    return `*${title}*\n_${url}_\n_${description}_`;
+  }).join('\n\n');
+  try {
+    const ss = `https://image.thum.io/get/fullpage/${url}`;
+    await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m);
+  } catch {
+    m.reply(msg);
+  }
+};
+handler.help = ['dni', 'dni'].map((v) => v + ' <pencarian>');
+handler.tags = ['internet'];
+handler.command = /^dni?$/i;
+export default handler;
+
+ let ss2 = await ssweb(url, 'desktop')
+let dataa = ss2.result
+async function ssweb(url, device = 'desktop'){
+return new Promise((resolve, reject) => {
+const base = 'https://www.screenshotmachine.com'
+const param = { url: url, device: device, cacheLimit: 0 }
+axios({url: base + '/capture.php', method: 'POST', data: new URLSearchParams(Object.entries(param)), headers: { 'content-type': 'application/x-www-form-urlencoded; charset=UTF-8' }}).then((data) => {
+const cookies = data.headers['set-cookie']
+if (data.data.status == 'success') {
+axios.get(base + '/' + data.data.link, { headers: { 'cookie': cookies.join('') }, responseType: 'arraybuffer' }).then(({ data }) => {
+let result = { status: 200, author: '@BrunoSobrino', result: data }
+resolve(result)})
+} else {
+reject({ status: 404, author: 'Ryzn', message: data.data })}}).catch(reject)})}*/
+ 
