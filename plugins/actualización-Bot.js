f80449ccecdc658f62f635702af0f4aace99db3a
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-04-20 10:08:28 -0400

    Actualizar actualización-Bot.js

--- plugins/actualización-Bot.js
+++ plugins/actualización-Bot.js
@@ -18,7 +18,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
     const document = doc[Math.floor(Math.random() * doc.length)];
-    const str = `┏━━━━━━🐲𝑳𝒖𝒄𝒌𝒚𝑩𝒐𝒕-𝑴𝑫🐲━━━━━━┓
+    const str = `┏━━━━━🐲𝑳𝒖𝒄𝒌𝒚𝑩𝒐𝒕-𝑴𝑫🐲━━━━┓
 ┃
 ┃ෆ _Tenemos novedades nuevas_
 ┃ෆ _en el bot Lucky tanto ya_
