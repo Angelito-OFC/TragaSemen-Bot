a1d9b1ab4da2737976dc63e9e3f6b2a1d427c032
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-04-29 14:51:20 -0400

    Actualizar rpg-minar_mysticcoins.js

--- plugins/rpg-minar_mysticcoins.js
+++ plugins/rpg-minar_mysticcoins.js
@@ -41,7 +41,7 @@ const handler = async (m, {conn, isPrems}) => { // lastmiming
 };
 handler.help = ['minar2'];
 handler.tags = ['gata'];
-handler.command = ['minar2', 'miming2', 'mine2', 'minarluckycoins', 'minarcoins', 'minarlucky'];
+handler.command = ['minar2', 'miming2', 'mine2', 'minarsemencoins', 'minarcoins', 'minarlucky'];
 handler.fail = null;
 handler.exp = 0;
 export default handler;
